eaf639e1be387473e10d2a3a85bec99feeb96d6b
Garrett Park <72951078+g2101@users.noreply.github.com>
2023-09-14 20:31:05 -0400

    finalized code and css design

---
+++ script.js
@@ -0,0 +1,24 @@
+function generateBBCode() {
+    const currentUTCDate = new Date();
+    const month = currentUTCDate.toLocaleString('default', { month: 'long', timeZone: 'UTC' }).toUpperCase();
+    const year = currentUTCDate.toLocaleString('default', { year: 'numeric', timeZone: 'UTC' }).toUpperCase();
+
+    const inputTextarea = document.getElementById('input-textarea');
+    let bbcode = `[altspoiler=${month} ${year} - OFFICER RECORD]\n`;
+
+    bbcode += inputTextarea.value;
+
+    bbcode += '\n[/altspoiler]';
+
+    document.getElementById('output-textarea').value = bbcode;
+}
+
+function copyToClipboard() {
+    const outputTextarea = document.getElementById('output-textarea');
+    const textarea = document.createElement('textarea');
+    textarea.value = outputTextarea.value;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+}
